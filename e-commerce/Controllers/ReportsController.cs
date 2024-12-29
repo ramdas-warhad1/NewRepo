749043405d9961bb13eb62c9127015bf50372412
Ramdas.Warhad <Ramdas.Warhad@PEP-PC-127>
2024-12-29 17:54:11 +0530

    removed cpmmented code

--- e-commerce/Controllers/ReportsController.cs
+++ e-commerce/Controllers/ReportsController.cs
@@ -10,12 +10,10 @@ public class ReportsController : Controller
     {
         _reportRepository = reportRepository;
     }
-    // GET: ReportsController
     public async Task<ActionResult> TopFiveSellingProducts(DateTime? sDate = null, DateTime? eDate = null)
     {
         try
         {
-            // by default, get last 7 days record
             DateTime startDate = sDate ?? DateTime.UtcNow.AddDays(-7);
             DateTime endDate = eDate ?? DateTime.UtcNow;
             var topFiveSellingProducts = await _reportRepository.GetTopNSellingProductsByDate(startDate, endDate);
